e4d24af5769358e89b2489db3680e8298e4e6408
Eryk J. <107011362+erykjj@users.noreply.github.com>
2026-07-03 19:22:13 -0400

    Clean up

--- main.ts
+++ main.ts
@@ -112,7 +112,7 @@ export default class TraverturePlugin extends Plugin {
                 const sorted = [...clauses].sort((a, b) => b[0].length - a[0].length);
                 
                 for (let i = 0; i < sorted.length; i++) {
-                    const [clauseText, _startPos, _endPos, ranges] = sorted[i];
+                    const [clauseText, , , ranges] = sorted[i];
                     const origIndex = clauses.indexOf(sorted[i]);
                     
                     if (origIndex === 0) {
