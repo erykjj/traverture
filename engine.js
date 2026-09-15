5707c530f4d6c19032a8685df7e481ae6659b225
Eryk J. <107011362+erykjj@users.noreply.github.com>
2026-09-15 18:41:04 -0400

    Update engine

--- engine.js
+++ engine.js
@@ -241,10 +241,10 @@ if (Symbol.dispose) TravertureEngine.prototype[Symbol.dispose] = TravertureEngin
 function __wbg_get_imports() {
     const import0 = {
         __proto__: null,
-        __wbg___wbindgen_throw_344f42d3211c4765: function(arg0, arg1) {
+        __wbg___wbindgen_throw_5d9e815e6fdf150f: function(arg0, arg1) {
             throw new Error(getStringFromWasm0(arg0, arg1));
         },
-        __wbindgen_cast_0000000000000001: function(arg0, arg1) {
+        __wbindgen_generic_0000000000000001: function(arg0, arg1) {
             // Cast intrinsic for `Ref(String) -> Externref`.
             const ret = getStringFromWasm0(arg0, arg1);
             return ret;
